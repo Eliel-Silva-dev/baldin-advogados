9090b40cfc4508cc0d45fdad5b5d5cca965ddb5e
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-23 12:26:09 -0300

    feat: react baldin advogados database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main id={styles.main_Home}>
-      <h2>Pagina inicial... lets goooooooooo</h2>
+      <h2>Pagina inicial...</h2>
     </main>
   );
 }
---
+++ src/database/AreasAtuacao/index.tsx
@@ -0,0 +1,183 @@
+const business = {
+  title: 'EMPRESARIAL/SOCIETÁRIO',
+  imgCard: '/img/imgCardBusiness',
+  imgAbout: '/img/imgAboutBusiness',
+  subAreas: [
+    'Recuperação de ativos',
+    'Reestruturação de passivos',
+    'Vendas e aquisições de Empresas – M&A',
+    'Criação de pessoas Jurídicas',
+    'Acordos de sócios',
+    'Marcas e patentes',
+  ],
+  infoSub: [
+    {
+      subTitle: 'Recuperação de ativos',
+      message: [
+        'Atuando ao lado dos clientes, o escritório Baldin Advogados possui expertise na gestão estratégica dos recursos para recuperação de ativos. Extrajudicial ou judicialmente, são utilizados os meios mais efetivos para que os clientes consigam receber quantias pendentes de pagamentos.',
+      ],
+    },
+    {
+      subTitle: 'Reestruturação de passivos',
+      message: [
+        'Em razão dos vários anos de experiência, o escritório consegue assessorar clientes em dificuldade financeira na tomada de decisões, reestruturação do passivo, rolamento de dívidas e renegociações, priorizando a proteção do patrimônio e a perpetuidade da atividade empresarial, trazendo de volta a rentabilidade e a saúde financeira.',
+      ],
+    },
+    {
+      subTitle: 'Vendas e aquisições de Empresas – M&A',
+      message: [
+        'Com sólidos conhecimentos para atuar tanto pelos compradores como pelos vendedores, conjugamos soluções negociais seguras com prática jurídica sólida para assessorar na compra e venda de empresas, participações societárias ou ativos, tanto como em incorporações, fusões e cisões decorrentes de reestruturação societária. Auxiliando em todo o processo, o escritório oferece suporte desde a negociação, passando pela redação dos instrumentos jurídicos iniciais (term sheet, mou), condução da due diligence jurídica, e redação dos instrumentos finais.',
+        'Aliando teoria e prática, protegemos seus clientes sem inviabilizar o negócio, estruturando alternativas operacionais e fixação de cláusulas para o aproveitamento positivo do negócio.',
+      ],
+    },
+    {
+      subTitle: 'Criação de pessoas Jurídicas',
+      message: [
+        'Buscando oferecer alternativas práticas e efetivas no relacionamento entre sócios e na própria exploração da atividade empresarial, o escritório cuida para que as empresas sejam constituídas da maneira mais segura possível, otimizando a forma societária e opção tributária, acompanhando desde a redação do contrato social completo até a redação de acordos de sócios / quotistas.',
+      ],
+    },
+    {
+      subTitle: 'Acordos de sócios',
+      message: [
+        'Atuando mais no dia-a-dia e rotina empresarial, prestamos suporte na redação e análise de contratos, típicos e atípicos, conferindo maior segurança e dinamicidade para as operações.',
+      ],
+    },
+    {
+      subTitle: 'Marcas e patentes',
+      message: [
+        'Com foco tanto no consultivo quanto no contencioso, o escritório suporta os clientes na pesquisa prévia, no registro/depósito de marcas/patentes, e mesmo em eventuais oposições administrativas ou discussões judiciais.',
+      ],
+    },
+  ],
+};
+const compliance = {
+  title: 'Compliance',
+  imgCard: '/img/imgCardComoliance',
+  imgAbout: '/img/imgAboutComoliance',
+  subAreas: [
+    'Desenvolvimento de regulamentos',
+    'Políticas anticorrupção',
+    'atentos para as principais leis internacionais',
+  ],
+  infoSub: [
+    {
+      subTitle: 'Atuação Compliance',
+      message: [
+        'Cada dia mais médias e grandes empresas estão buscando políticas transparentes e seguras na governança corporativa, relação com investidores e com órgãos públicos, buscando a adaptação interna de normas internacionais de compliance e gestão de riscos.',
+        'Coube, portanto, ao escritório Baldin Advogados cuidar para desenvolver regulamentos e políticas anticorrupção nesse mercado emergente, de olhos atentos para as principais leis internacionais (FCPA, Bribery Act, Sarbanes-Oxley – SOX, Dodd-Frank ACT, etc.), buscando uma relação harmoniosa, sadia e proba entre ambiente corporativo, órgãos públicos e mercado financeiro.',
+      ],
+    },
+  ],
+};
+const labor = {
+  title: 'TRABALHISTA',
+  imgCard: '/img/imgCardLabor',
+  imgAbout: '/img/imgAboutLabor',
+  subAreas: [
+    'Consultivo',
+    'Contencioso',
+    'Recuperação de Depósitos Judiciais e Recursais em Processos Arquivados',
+  ],
+  infoSub: [
+    {
+      subTitle: 'Consultivo',
+      message: [
+        'Na área consultiva, o escritório presta assessoria na elaboração de contratos de trabalho, pareceres jurídicos referentes às relações de trabalho, identificando os riscos a que o cliente está exposto, evitando possíveis demandas judiciais.',
+        'Além disso, o escritório é apto a desenvolver programas de demissão voluntária e programas de incentivo a empregados, bem como realizar auditorias legais nas mais diversas operações, identificando as práticas que possam acarretar em contingências futuras e avaliando os riscos presentes nos processos em curso.',
+      ],
+    },
+    {
+      subTitle: 'Contencioso',
+      message: [
+        'Na área contenciosa, o escritório atua na defesa dos interesses de pessoas jurídicas (empresas públicas ou privadas, nacionais ou estrangeiras) em todas as instâncias judiciais, em dissídios individuais e coletivos.',
+        'Atuamos também em instâncias administrativas da fiscalização pelo Ministério do Trabalho e nos procedimentos perante o Ministério Público do Trabalho, realizando audiências e sustentações orais em todo o território nacional, além de traçar e executar reestruturação trabalhista decorrente de fusões, aquisições ou reorganizações societárias.',
+      ],
+    },
+    {
+      subTitle:
+        'Recuperação de Depósitos Judiciais e Recursais em Processos Arquivados',
+      message: [
+        'É comum em relações processuais que, a depender do tipo de processo, as pessoas físicas e jurídicas sejam obrigadas a depositar valores em conta judicial (ex. embargos à execução fiscal, depósito recursal). Em alguns casos ocorre o arquivamento do processo sem que tais valores sejam liberados a quem de direito, ou mesmo sem que a correção monetária do depósito seja liberada.',
+        'Buscando solucionar esse problema o escritório desenvolveu ferramentas para recuperar esse ativo para as empresas, contando com softwares especializados e mão-de-obra treinada para a minuciosa pesquisa, recuperando muitas vezes quantias vultuosas que estavam fora do radar da empresa.',
+      ],
+    },
+  ],
+};
+const realState = {
+  title: 'IMOBILIÁRIO',
+  imgCard: '/img/imgCardRealState',
+  imgAbout: '/img/imgAboutRealState',
+  subAreas: ['Consultivo', 'Contencioso'],
+  infoSub: [
+    {
+      subTitle: 'Consultivo',
+      message: [
+        'Mesmo em tempos de crise, o imóvel é sempre o melhor investimento. Atento para esse setor robusto, rentável e com enorme capacidade de crescimento, o escritório Baldin Advogados adquiriu as capacidades técnicas necessárias para auxiliar negocial e juridicamente tanto incorporadoras no lançamento de empreendimentos (estruturando-os) quanto unidades familiares que possuem a gleba de terra (e que intencionam explorá-la na forma de loteamento).',
+        'Some-se ao descritivo acima a assessoria à imobiliárias na gestão de seus contratos e viabilização de operações imobiliárias de compra e venda.',
+      ],
+    },
+    {
+      subTitle: 'Contencioso',
+      message: [
+        'Decorrência natural do exercício consultivo, estamos sempre procurado para patrocinar seus clientes em demandas envolvendo questões imobiliárias/ambientais, apresentando elevado índice de êxito.',
+      ],
+    },
+  ],
+};
+const tax = {
+  title: 'TRIBUTÁRIO',
+  imgCard: '/img/imgCardTax',
+  imgAbout: '/img/imgAboutTax',
+  subAreas: [
+    'Reestruturação tributária',
+    'Consultas tributárias',
+    'Planejamento tributário',
+    'Análise de enquadramento tributário',
+    'Segregação de operação',
+    'Análise e mudança de operação',
+    'Recuperação de depósitos recursais',
+  ],
+  infoSub: [
+    {
+      subTitle: 'Consultivo',
+      message: [
+        'Reestruturação tributária, consultas tributárias, planejamento tributário, análise de enquadramento tributário, segregação de operação, análise e mudança de operação.',
+        'Essas são apenas algumas as opções consultivas oferecidas pelo escritório Baldin Advogados para seus clientes, com vistas a otimizar o recolhimento de tributos utilizando as faculdades previstas em lei.',
+      ],
+    },
+    {
+      subTitle: 'Contencioso',
+      message: [
+        'Contencioso administrativo ou judicial, municipal, estadual ou federal, o escritório reúne as competências necessárias para postular e/ou defender na esfera administrativa ou judicial os interesses dos clientes, seja buscando declaração negativa, anulação ou mesmo elaborando defesas de autos de infração.',
+      ],
+    },
+    {
+      subTitle: 'Soluções pontuais',
+      message: [
+        'Sempre atualizado nas decisões dos tribunais superiores e nas mudanças de entendimentos pelas cortes administrativas e órgãos de fiscalização, estamos sempre oferecendo aos clientes novas teses (conservadoras ou não), buscando municiar o empresário de elementos para a melhor consecução da atividade.',
+      ],
+    },
+    {
+      subTitle: 'Recuperação de crédito',
+      message: [
+        'Com foco na análise crítica dos últimos cinco anos da atividade desenvolvida, a recuperação de crédito se apresenta como uma alternativa real para o cliente de tanto rever seus procedimentos fiscais e contábeis como de identificar um ativo até então não mapeado.',
+      ],
+    },
+  ],
+};
+const civilLitigation = {
+  title: 'Contencioso Cível',
+  imgCard: '/img/imgCardCivilLitigation',
+  imgAbout: '/img/imgAboutCivilLitigation',
+  subAreas: ['Consumidor', 'Responsabilidade civil', 'Contratos em geral'],
+  infoSub: [
+    {
+      subTitle: 'Contencioso Cível',
+      message: [
+        'Depois de anos de sucesso o segmento contencioso cível do Baldin Advogados se consolidou como um dos mais importantes e efetivos no cenário jurídico no eixo Campinas-São Paulo, contando com a visão estratégia da equipe somada ao conhecimento jurídico adquirido ao longo de reconhecida carreira teórica.',
+        'Apto a receber e administrar carteiras de processo envolvendo as mais diversas áreas (consumidor, responsabilidade civil, contratos em geral, etc.), o contencioso cível do Baldin Advogados não busca apenas diminuir perdas, mas principalmente atuar na recuperação da credibilidade e do posicionamento dos clientes.',
+      ],
+    },
+  ],
+};
+export { business, compliance, labor, realState, tax, civilLitigation };
