25c50b277d90f149e090811a83898ab17e891111
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-23 16:31:34 -0300

    feat: react baldin advogados page home

---
+++ src/app/civillitigation/page.tsx
@@ -0,0 +1,11 @@
+import style from './style.min.module.css';
+
+const civilLitigation = () => {
+  return (
+    <main id={style.main_civilLitigation}>
+      <h2>Pagina de Tax...</h2>
+    </main>
+  );
+};
+
+export default civilLitigation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import HeaderSection from '@/components/HeaderSection';
 import style from './page.module.css';
+import areasAtuacao from '@/database/AreasAtuacao';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,7 +11,26 @@ export default function Home() {
         message="Soluções jurídicas, negociais, e estratégicas focadas no resultado!"
       />
       <main id={style.main_Home}>
-        <h2>Pagina inicial...</h2>
+        <h2>Nossos Serviços</h2>
+        <p>Veja como podemos ajudá-lo</p>
+        <div className={style.container_atuacao}>
+          {areasAtuacao &&
+            areasAtuacao.map((area, idx) => (
+              <div key={idx} className={style.atuacao_Card}>
+                <div className={style.img_card}>
+                  <img src={area.imgCard} alt={area.title} />
+                </div>
+                <div className={style.info_card}>
+                  <h3>{area.title}</h3>
+                  <div>
+                    {area.subAreas &&
+                      area.subAreas.map((sub, idx) => <p key={idx}>- {sub}</p>)}
+                  </div>
+                  <Link href={area.href}>Saiba mais</Link>
+                </div>
+              </div>
+            ))}
+        </div>
       </main>
     </>
   );
