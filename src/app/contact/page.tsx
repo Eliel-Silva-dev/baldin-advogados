2b90e0aa5033ed264615754d92002a1e6f995951
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-23 19:15:56 -0300

    feat: react baldin advogados component fomr

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,48 @@
+'use client';
+
+import HeaderDiv from '@/components/HeaderDiv';
 import style from './style.min.module.css';
+import FormContact from '@/components/FormContact';
 
 const Contact = () => {
   return (
-    <main id={style.main_Contact}>
-      <h2>Pagina de Contact...</h2>
-    </main>
+    <>
+      <HeaderDiv title="Contact" />
+      <main id={style.main_Contact}>
+        <section className={style.contact_address}>
+          <div>
+            <h2>Contato</h2>
+            <p>Conte com a experiência de nossa equipe.</p>
+          </div>
+          <div>
+            <h3>Endereço:</h3>
+            <p>
+              R. Oscár Alves Costa, 177 Jd. Santa Genebra II (Barão Geraldo) -
+              Campinas – SP, 13084-762
+            </p>
+          </div>
+          <div>
+            <h3>Celular/WhatsApp:</h3>
+            <p>
+              <span>+55 19 9 9856-5139</span>
+            </p>
+          </div>
+          <div>
+            <h3>Telefone</h3>
+            <p>
+              <span>+55 19 3291.6417</span>
+            </p>
+          </div>
+          <div>
+            <h3>E-mail</h3>
+            <p>
+              <span>contato@baldinadv.com.br</span>
+            </p>
+          </div>
+        </section>
+        <FormContact />
+      </main>
+    </>
   );
 };
 
