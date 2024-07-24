33eed0476217c128553841d1274625138f7b95a1
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-24 11:25:03 -0300

    fix: react baldin advogados add tel

--- src/components/buttons/ButtonWhatsapp/index.tsx
+++ src/components/buttons/ButtonWhatsapp/index.tsx
@@ -5,7 +5,12 @@ import Link from 'next/link';
 const ButtonWhatsapp = () => {
   return (
     <div id={style.whats}>
-      <Link href={'tel:+55199'}>
+      <Link
+        href={
+          'https://api.whatsapp.com/send/?phone=5519998565139&text=Ol%C3%A1+Baldin+Advogados%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+os+seus+servi%C3%A7os.&type=phone_number&app_absent=0'
+        }
+        target='_blank'
+      >
         <FaWhatsapp />
       </Link>
     </div>
