5c9ebb79f1b6974406d804fc88ce3bb5e85c8fc0
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-23 23:27:50 -0300

    feat: react baldin advogados page compliance

--- src/app/compliance/page.tsx
+++ src/app/compliance/page.tsx
@@ -1,9 +1,64 @@
+'use client';
+
+import ContactSection from '@/components/ContactSection';
 import style from './style.min.module.css';
+import areasAtuacao from '@/database/AreasAtuacao';
 
 const Compliance = () => {
+  const complianceData = areasAtuacao.filter(
+    (area) => area.title == 'Compliance',
+  );
+  const activeCard = (el: HTMLElement) => {
+    const elPai = el.parentNode as HTMLElement;
+    const subTitle = document.querySelectorAll(
+      '.style_min_active_sub__iLxft',
+    ) as NodeList;
+
+    if (!elPai.classList.contains('style_min_active_sub__iLxft')) {
+      subTitle.forEach((sub) => {
+        const subT = sub as HTMLElement;
+        subT.classList.remove('style_min_active_sub__iLxft');
+      });
+
+      elPai.classList.add('style_min_active_sub__iLxft');
+    } else {
+      elPai.classList.remove('style_min_active_sub__iLxft');
+    }
+  };
   return (
     <main id={style.main_Compliance}>
-      <h2>Pagina de Compliance...</h2>
+      {complianceData &&
+        complianceData.map((area, idx) => (
+          <section key={idx}>
+            <div className={style.container_image}>
+              <img src={area.imgCard} alt={area.title} />
+            </div>
+            <div className={style.container_info}>
+              <h2>{area.title}</h2>
+              <div className={style.container_card_compliance}>
+                {area.subAreas &&
+                  area.infoSub.map((infoSub, idx) => (
+                    <div key={idx} className={style.card_compliance}>
+                      <div className={style.subtitle_compliance}>
+                        <h3
+                          onClick={(e) => activeCard(e.target as HTMLElement)}
+                        >
+                          {infoSub.subTitle}
+                        </h3>
+                      </div>
+                      <div className={style.message_compliance}>
+                        {infoSub.message &&
+                          infoSub.message.map((mess, idx) => (
+                            <p key={idx}>{mess}</p>
+                          ))}
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </section>
+        ))}
+      <ContactSection />
     </main>
   );
 };
